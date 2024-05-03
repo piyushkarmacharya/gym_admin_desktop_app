04f4cf2d7ee7b23e8b6f6dcc7a7fdda2e5fbb14d
piyushkarmacharya <Karmacharyapiyush65@gmail.com>
2024-05-03 10:44:39 +0545

    connected Api for update and read the Attendance Qr

--- lib/pages/attendance_qr.dart
+++ lib/pages/attendance_qr.dart
@@ -2,6 +2,8 @@ import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:flutter/widgets.dart";
 import "package:qr_flutter/qr_flutter.dart";
+import "package:http/http.dart" as http;
+import 'dart:convert';
 
 class AttendanceQr extends StatefulWidget {
   const AttendanceQr({super.key});
@@ -11,6 +13,49 @@ class AttendanceQr extends StatefulWidget {
 class _AttendanceQrState extends State<AttendanceQr> {
   String str = "";
   TextEditingController ctr = TextEditingController();
+
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      String url = 'http://127.0.0.1:8000/api/AttendanceQr';
+      final res = await http.get(Uri.parse(url));
+      if (res.statusCode == 200) {
+        List data = jsonDecode(res.body);
+        await Future.delayed(Duration(seconds: 2));
+        setState(() {
+          str = data[0]["qrstr"];
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<void> updateQr() async {
+    final Map<String, dynamic> data = {
+      "qrstr":str,
+    };
+    final Response = await http.post(
+      Uri.parse("http://127.0.0.1:8000/api/AttendanceQr/update"),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(data),
+    );
+    if (Response.statusCode == 200) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("updated")));
+      
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Failed")));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -29,19 +74,29 @@ class _AttendanceQrState extends State<AttendanceQr> {
               Expanded(
                   flex: 3,
                   child: Container(
-                    child: QrImageView(data: str),
+                    child: str == ""
+                        ? CircularProgressIndicator(
+                            backgroundColor: Colors.grey,
+                          )
+                        : QrImageView(
+                            data: str,
+                            version: QrVersions.auto,
+                          ),
                   )),
               Spacer(),
               Expanded(
                 flex: 1,
                 child: Row(
                   children: [
-                    Spacer(flex: 3,),
+                    Spacer(
+                      flex: 3,
+                    ),
                     Expanded(
                       flex: 2,
                       child: TextField(
+                        obscureText: true,
                         decoration: InputDecoration(
-                          label:Text("Enter a string"),
+                          label: Text("Enter a string"),
                           contentPadding:
                               EdgeInsets.symmetric(vertical: 0, horizontal: 10),
                           border: OutlineInputBorder(
@@ -51,20 +106,24 @@ class _AttendanceQrState extends State<AttendanceQr> {
                         controller: ctr,
                       ),
                     ),
-                    Spacer(flex: 1,),
+                    Spacer(
+                      flex: 1,
+                    ),
                     Expanded(
                       flex: 1,
                       child: ElevatedButton(
                         onPressed: () {
                           setState(() {
-                            str=ctr.text;
+                            str = ctr.text;
+                            updateQr();
                           });
-                          
                         },
                         child: Text("Generate"),
                       ),
                     ),
-                    Spacer(flex: 3,),
+                    Spacer(
+                      flex: 3,
+                    ),
                   ],
                 ),
               ),
