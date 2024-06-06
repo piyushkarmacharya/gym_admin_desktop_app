6de8d1cc2ce6399065caa7eb9774df1c1c8a6fd8
piyushkarmacharya <Karmacharyapiyush65@gmail.com>
2024-06-06 08:35:07 +0545

    changed ui of feedback page

--- lib/pages/feedbackPage.dart
+++ lib/pages/feedbackPage.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -16,8 +17,7 @@ class _FeedbackPageState extends State<FeedbackPage> {
       final res = await http.get(Uri.parse(url));
       if (res.statusCode == 200) {
         setState(() {
-          List data = jsonDecode(res.body);
-          print(data);
+          data = jsonDecode(res.body);
         });
       } else {
         print("error in connection");
@@ -26,14 +26,59 @@ class _FeedbackPageState extends State<FeedbackPage> {
       print(e);
     }
   }
-void initState(){
-  super.initState();
-  getFeedback();
-}
+
+  void initState() {
+    super.initState();
+    getFeedback();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    print(data);
     return Scaffold(
-      body: Text("hello"),
+      body: data.length == 0
+          ? CircularProgressIndicator()
+          : Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+                height: double.infinity,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                        height: 0.2*screenHeight,
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(20,8,8,8),
+                            child: Row(
+                              children: [
+                                Text(data[index]['date'].toString()),
+                                Expanded(
+                                   
+                                    child: Padding(
+                                      padding: const EdgeInsets.fromLTRB(20,0,0,0),
+                                      child: SingleChildScrollView(
+                                          scrollDirection: Axis.vertical,
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("By ${data[index]['name'].toString()} (${data[index]['mid'].toString()}): "),
+                                              Text("- ${data[index]['feedback'].toString()}"
+                                                  ),
+                                            ],
+                                          )),
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ));
+                  },
+                ),
+              ),
+          ),
     );
   }
 }
